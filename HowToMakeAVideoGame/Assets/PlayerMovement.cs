2e5c2bf08fec756cc884a4affc0faac3f87d2f80
noremak89 <35876318+noremak89@users.noreply.github.com>
2018-07-26 21:27:09 -0400

    forgot a separate branch part 3

--- HowToMakeAVideoGame/Assets/PlayerMovement.cs
+++ HowToMakeAVideoGame/Assets/PlayerMovement.cs
@@ -3,15 +3,28 @@
 public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
        
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        //add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
+
     }
 }
