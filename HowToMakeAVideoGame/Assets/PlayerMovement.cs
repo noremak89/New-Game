d012913975503d16c0d59167717de074697223d3
noremak89 <35876318+noremak89@users.noreply.github.com>
2018-07-26 21:01:24 -0400

    finished part2

---
+++ HowToMakeAVideoGame/Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
